79ff4841e853c4374e5fccf666bfcadd030fd428
LordRavAlon <122441699+LordRavAlon@users.noreply.github.com>
2023-03-19 18:59:54 +0100

    Sliding made
    Etape 4 réalisée

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,47 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
-function test () {
-alert  ("Test Gauche") ;
-} 
 
-const flecheGauche = document.getElementById("button_left") ;
-flecheGauche.addEventListener("click", test ) ;
+const image = document.querySelector("#banner .banner-img");
+const tagLine = document.querySelector("#banner p");
+const dots = document.querySelectorAll("#banner .dots .dot");
+let i=0;
 
-function test2 () {
-	alert  ("Test Droite") ;
-	} 
+function moveLeft() {
+	dots[i].classList.remove("dot_selected");
+	i--;
+	dots[i].classList.add("dot_selected");
+	image.src = "./assets/images/slideshow/" + slides[i].image;
+	tagLine.innerHTML = slides[i].tagLine;
 	
-	const flecheDroite = document.getElementById("button_right") ;
-	flecheDroite.addEventListener("click", test2 ) ;
+}
+
+const flecheGauche = document.getElementById("button_left");
+flecheGauche.addEventListener("click", moveLeft);
+
+function moveRight() {
+	dots[i].classList.remove("dot_selected");
+	i++;
+	dots[i].classList.add("dot_selected");
+	image.src = "./assets/images/slideshow/" + slides[i].image;
+	tagLine.innerHTML = slides[i].tagLine;
+}
+
+const flecheDroite = document.getElementById("button_right");
+flecheDroite.addEventListener("click", moveRight);
